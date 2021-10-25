d8a7cb37581128b230c183c8f520f90b96d6742e
Dmitry <chuksin.dl@defbyte.ru>
2021-10-25 22:37:05 +0300

    modify GET request reaction

--- prnsrv.py
+++ prnsrv.py
@@ -4,45 +4,33 @@ Very simple HTTP server in python for logging requests
 Usage::
     ./server.py [<port>]
 """
-import html
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import io
 import logging
 import cups
 import base64
 import urllib.parse as urlparse
-"""from jinja2 import Environment, PackageLoader, select_autoescape
-import jinja2
-
-loader=jinja2.FileSystemLoader('./')
-
-env = Environment(
-
-)"""
+import markdown
 
 class S(BaseHTTPRequestHandler):
     def log_message(self, format: str, *args):
         logging.info('Request: {0}'.format(args))
     def _set_response(self, http_code, content_type):
         self.send_response(http_code)
-        self.send_header('Content-type', content_type)
+        self.send_header('Content-type', ''.join([content_type,'; charset=utf-8']))
         self.end_headers()
     
     def do_GET(self):        
-        if self.path == '/':
-            get_ManPage(self)
-            return
         if self.path == '/favicon.ico':
             return
         parsed_path = urlparse.urlparse(self.path)
         queryParams = urlparse.parse_qs(parsed_path.query)
-        keys = queryParams.keys()
-        if 'command' not in keys:
-            get_ManPage(self)
-            return
-        command = queryParams.get('command')
-        logging.debug(command)
-        
+        logging.debug(queryParams)
+        data = commandSelector(queryParams)
+        self._set_response(200, 'text/html')
+        logging.debug(data)
+        self.wfile.write(bytes(data.encode('utf-8')))                
+                
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
@@ -55,8 +43,7 @@ class S(BaseHTTPRequestHandler):
                     self._set_response(400)
                     self.wfile.write('Printer not specified'.format(content_length).encode('utf-8'))
                     return
-                cups.setServer('localhost')
-                conn = cups.Connection()
+                conn = cups.Connection(host='localhost')
                 try:
                     id = conn.createJob(queryParams['printer_name'][0], '1C', { 'document-format':cups.CUPS_FORMAT_PDF })
                     conn.startDocument(queryParams['printer_name'][0], id, '1C', cups.CUPS_FORMAT_PDF, 1)
@@ -74,18 +61,40 @@ class S(BaseHTTPRequestHandler):
         self._set_response(200,'application/json')
         self.wfile.write('Print document succesfully'.format(content_length).encode('utf-8'))
 
-def get_ManPage(getHandler):
-    getHandler._set_response(200, 'text/html')
-    doc, tag, text = Doc().tagtext()
-    with tag('html'):
-        with tag('header'):
-            doc.attr(klass = 'normal-style')
-        with tag('body'):
-            doc.attr(klass = 'normal-style')
-            text('1C-to-CUPS service')        
-        webdoc = doc.getvalue()
-    getHandler.wfile.write('{}'.format(webdoc).encode('utf-8'))
+def getHelpPage(args):
+    text = ''
+    with open("README.md", "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+    return markdown.markdown(text, output_format='html')
+    
+def getPrinterStatus(args):
+    conn = cups.Connection(host='localhost')
+    printer_name = args.get('printer_name', None)
+    if printer_name == None:
+        logging.debug('printer_name parameter is required')
+        return ''
+    if len(printer_name) == 1:
+        attr = conn.getPrinterAttributes(printer_name[0].encode('utf-8'))
+    else:
+        logging.critical('Too many "printer_name" parameters')
+        attr = getHelpPage(args)
+    return attr
 
+def commandSelector(args):
+    switcher = {
+        'get_printer_status': getPrinterStatus,
+        'get_help_page': getHelpPage
+    }
+        
+    command = args.get('command', 'get_help_page')
+    if len(command) == 1:
+        func = switcher.get(str(command[0]), getHelpPage)
+        data = func(args)
+    else:
+        logging.critical('Too many "command" parameters')
+        data = getHelpPage(args)
+    return data 
+    
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(level=logging.DEBUG,
     format='[%(asctime)s] [%(levelname)s] %(message)s',
